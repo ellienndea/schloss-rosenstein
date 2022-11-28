a09f2048ed4aaed522af0c8e483d7e19535edae5
nuJane <50409910+nuJane@users.noreply.github.com>
2022-11-28 19:04:18 +0100

    add function to check code input

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ const sendButton = document.getElementById("send-btn");
 
 codeInput.addEventListener("focus", ()=> {codeInput.setAttribute("placeholder", "")});
 codeInput.addEventListener("blur", ()=> {codeInput.setAttribute("placeholder", "*****")});
+
+function codeCheck(){
+    if(codeInput.value === "14075"){
+        /* replace w message appears */
+        alert(" Lieber Konrad, Dein Lieblingsrezept findst Du hier: Deine Hildergard")
+    } else {
+        /* replace shake animation */
+        alert("Leider falsch")
+    }
+}
+
+sendButton.addEventListener("click",()=>{
+    codeCheck();
+});
+
+/* call codeCheck function with enter key */
+codeInput.addEventListener('keyup', (event) => {
+    const keyName = event.key;
+    if (keyName === 'Enter') {
+    codeCheck();
+    }
+});
+/* end call codeCheck function with enter key */
+
+
